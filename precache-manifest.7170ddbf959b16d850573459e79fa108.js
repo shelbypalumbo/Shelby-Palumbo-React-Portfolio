c1852a27599f087be50fe12d82964ed762c9f38f
shelby palumbo <shelbypalumbo94@gmail.com>
2020-04-13 23:23:14 -0400

    Updates

---
+++ precache-manifest.7170ddbf959b16d850573459e79fa108.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2784ff2629c1ce05d2594fb95c16eaeb",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/index.html"
+  },
+  {
+    "revision": "6f27d30b733d29eaf141",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/css/main.b2b90a8c.chunk.css"
+  },
+  {
+    "revision": "6a01ccc1ededb89ae862",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/js/2.725e3fc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/js/2.725e3fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f27d30b733d29eaf141",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/js/main.388bf844.chunk.js"
+  },
+  {
+    "revision": "8d98a551c34a02a9eba8",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/js/runtime-main.7cd61bf9.js"
+  },
+  {
+    "revision": "917efa294830f811ddedc3bbb2b989d4",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/media/fern.917efa29.jpg"
+  },
+  {
+    "revision": "659a150469018b9d6493016a97783a0f",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/media/fern_2.659a1504.jpg"
+  },
+  {
+    "revision": "2c7032e5d2c34facb4e0d1e7568e8920",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/media/gitHub.2c7032e5.png"
+  },
+  {
+    "revision": "bf9f6cdaccfa2c04e3e74fbd6424dad6",
+    "url": "/shelbypalumbo/Shelby-Palumbo-React-Portfolio/static/media/headshot2.bf9f6cda.png"
+  }
+]);
