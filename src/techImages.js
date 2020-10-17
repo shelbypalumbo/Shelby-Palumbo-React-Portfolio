0adf60c372d537298da8d12cb28634f53eb54eac
shelby palumbo <shelbypalumbo94@gmail.com>
2020-10-16 21:29:03 -0400

    Adding images to tech experience and updated resume

--- src/techImages.js
+++ src/techImages.js
@@ -5,46 +5,50 @@ export default [
   },
   {
     id: 1,
-    image: require("./images/techImages/vin250.png")
+    image: require("./images/techImages/47_systems.png")
   },
   {
     id: 2,
-    image: require("./images/techImages/u47.jpg")
+    image: require("./images/techImages/c12_chasis.png")
   },
   {
     id: 3,
-    image: require("./images/techImages/transplate.jpg")
-  },
-  {
-    id: 4,
-    image: require("./images/techImages/251.jpg")
-  },
-  {
-    id: 5,
-    image: require("./images/techImages/vf14k.jpg")
-  },
-  {
-    id: 6,
-    image: require("./images/techImages/mylar.jpg")
-  },
-  {
-    id: 7,
-    image: require("./images/techImages/cutMylar.png")
-  },
-  {
-    id: 8,
     image: require("./images/techImages/lap.jpg")
   },
   {
+    id: 4,
+    image: require("./images/techImages/u47.jpg")
+  },
+  {
+    id: 5,
+    image: require("./images/techImages/transplate.jpg")
+  },
+  {
+    id: 6,
+    image: require("./images/techImages/251.jpg")
+  },
+  {
+    id: 7,
+    image: require("./images/techImages/vf14k.jpg")
+  },
+  {
+    id: 8,
+    image: require("./images/techImages/mylar.jpg")
+  },
+  {
     id: 9,
-    image: require("./images/techImages/me.png")
+    image: require("./images/techImages/cutMylar.png")
   },
   {
     id: 10,
-    image: require("./images/techImages/c12.JPG")
+    image: require("./images/techImages/me.png")
   },
   {
     id: 11,
-    image: require("./images/techImages/WFHsetUp.JPG")
+    image: require("./images/techImages/c12.JPG")
+  },
+  {
+    id: 12,
+    image: require("./images/techImages/251repair.png")
   }
 ];
